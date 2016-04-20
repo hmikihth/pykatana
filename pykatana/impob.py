c4d64f878ecb4bd2b2672c47bbc735be49f10e25
hmikihth <szubotaj@gmail.com>
2016-04-20 01:56:52 +0100

    impob has been checked

--- pykatana/impob.py
+++ pykatana/impob.py
@@ -2,32 +2,32 @@
 #-*- coding:utf-8 -*-
 
 class ImpOb():
-  def __init__(self, name):
-      self.set_name(name)
-      self.parentname = None
-      self.parent_impob = None
-      self.parentname = None
+    def __init__(self, name):
+        self.set_name(name)
+        self.parentname = None
+        self.parent_impob = None
+        self.parentname = None
 
 
-  def set_name(self, name):
-      self.name = name
+    def set_name(self, name):
+        self.name = name
 
 
-  def set_source(self, source):
-      self.source = source
+    def set_source(self, source):
+        self.source = source
       
       
-  def set_parentname(self, parentname):
-      self.parentname = parentname
-
+    def set_parentname(self, parentname):
+        self.parentname = parentname
+       
   
-  def set_parentname(self, parentname):
-      self.parentname = parentname  
+    def set_parentname(self, parentname):
+        self.parentname = parentname  
       
       
-  def find_parent(self, objects):
-      if self.parentname is not None:
-        for obj in objects:
-            if obj.name == self.parentname:
-               self.add_related_object(obj)
+    def find_parent(self, objects):
+       if self.parentname is not None:
+           for obj in objects:
+               if obj.name == self.parentname:
+                   self.add_related_object(obj)
                
--- tests/test_impob.py
+++ tests/test_impob.py
@@ -6,23 +6,22 @@ import unittest
 from pykatana.impob import ImpOb
 
 class TestImpOb(unittest.TestCase):
-  def setUp(self):
-      self.obj = ImpOb("myOb")
+    def setUp(self):
+        self.obj = ImpOb("myOb")
 
-  def test_set_name(self):
-      name = "myname"
-      self.obj.set_name(name)
-      self.assertEqual(name, self.obj.name)
+    def test_set_name(self):
+        name = "myname"
+        self.obj.set_name(name)
+        self.assertEqual(name, self.obj.name)
 
 
-  def test_set_source(self):
-      source = "mysource"
-      self.obj.set_source(source)
-      self.assertEqual(source, self.obj.source)
+    def test_set_source(self):
+        source = "mysource"
+        self.obj.set_source(source)
+        self.assertEqual(source, self.obj.source)
       
       
-  def test_set_parentname(self):
-      parentname = "parent"
-      self.obj.set_parentname(parentname)
-      self.assertEqual(self.obj.parentname, parentname)
-  
+    def test_set_parentname(self):
+        parentname = "parent"
+        self.obj.set_parentname(parentname)
+        self.assertEqual(self.obj.parentname, parentname)
